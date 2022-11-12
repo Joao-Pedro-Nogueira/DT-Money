affd201bf49fbe8dd8eaf38a527eab497b9677f8
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-11-12 18:02:35 -0400

    Cálculo de resumo de transações configurado (valores não formatados em moeda)

--- src/components/Header/Summary/index.tsx
+++ src/components/Header/Summary/index.tsx
@@ -5,10 +5,29 @@ import { useContext } from "react";
 import { TransactionsContext } from "../../../contexts/TransactionsContext";
 
 export function Summary() {
+  
+  const theme = useTheme()
 
   const { transactions } = useContext(TransactionsContext)
 
-  const theme = useTheme()
+  const incomeTransactions = transactions.filter((transactions) => {
+    return transactions.type === 'income'
+  } )
+
+  const totalIncomePrice = incomeTransactions.reduce((accumulator, incomeTransaction ) => {
+    return accumulator + incomeTransaction.price
+  }, 0)
+
+  const outcomeTransactions = transactions.filter((transactions) => {
+    return transactions.type === 'outcome'
+  })
+
+  const totalOutcomePrice = outcomeTransactions.reduce((accumulator, outcome) => {
+    return accumulator + outcome.price
+  },0)
+
+  const totalResultPrice = totalIncomePrice - totalOutcomePrice
+
   return(
     <SummaryContainer className="container">
       <SummaryCard>
@@ -16,21 +35,21 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={23} color={theme["green-300"]} />
         </header>
-        <span>R$ 17.400,00</span>
+        <span>{totalIncomePrice}</span>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={23} color={theme["red-300"]} />
         </header>
-        <span>R$ 17.400,00</span>
+        <span>{totalOutcomePrice}</span>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <CurrencyDollar size={23} color={theme["white"]} />
         </header>
-        <span>R$ 17.400,00</span>
+        <span>{totalResultPrice}</span>
       </SummaryCard>
     </SummaryContainer>
   )
