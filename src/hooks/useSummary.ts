3735a4ea052754ff8afdbe367f9da2bfa2c1e775
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-11-17 10:42:21 -0400

    Hook useCallback aplicado ao contexto e useMemo ao useSummary

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,29 +1,32 @@
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactionsContext'
+import { useMemo } from 'react'
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    function (accumulator, transaction) {
-      if (transaction.type === 'income') {
-        accumulator.income += transaction.price
-      } else {
-        accumulator.outcome += transaction.price
-      }
+  const summary = useMemo(() => {
+    transactions.reduce(
+      function (accumulator, transaction) {
+        if (transaction.type === 'income') {
+          accumulator.income += transaction.price
+        } else {
+          accumulator.outcome += transaction.price
+        }
 
-      accumulator.total = accumulator.income - accumulator.outcome
+        accumulator.total = accumulator.income - accumulator.outcome
 
-      return accumulator
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return accumulator
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
